96b2a61fd30a4f7910f7778e5813a2cc51e6a3fc
Paul Anderson <paul.anderson@proactis.com>
2018-09-12 09:27:44 +0100

    updated constraints scripts to correctly join default to column

--- scripts/schema-info/find-table-column-constraints.sql
+++ scripts/schema-info/find-table-column-constraints.sql
@@ -5,9 +5,11 @@
 --	Detail:		
 --
 -------------------------------------------------------------------------------
+declare @useLike bit
 declare @columnName varchar(max)
-declare @tableName varchar(max)
+declare @tableName varchar(max) = 'User'
 declare @colType varchar(max)
+declare @constraintName varchar(max)
 
 
 select	  
@@ -21,17 +23,23 @@ select
 		, con.[modify_date] as [ConstraintModified]
 		
 from sys.default_constraints con
-join sys.columns col on  con.parent_column_id = col.column_id  
+inner join sys.columns col ON col.default_object_id = con.[object_id]
+inner join sys.objects o ON o.[object_id] = con.parent_object_id 
 join sys.tables tbl on col.[object_id] = tbl.[object_id]
 join sys.types t on t.[user_type_id] = col.[user_type_id]
 
 where
 	1=1
 	and (@colType is null or t.[name] = @colType)
-	and (@tableName is null or tbl.[name] like '%' + @tableName + '%')
+	and (@tableName is null 
+			or ((IsNull(@useLike, 0) = 1 and tbl.[name] like '%' + @tableName + '%') 
+				or (tbl.[name] = @tableName)
+			)
+		)
 	and (@columnName is null or col.[name] like '%' + @columnName + '%')
+	and (@constraintName is null or con.[name] like '%' + @constraintName + '%')
 
 order by 
 	  tbl.[name]
 	, col.[name]
-
+	, con.[name]
