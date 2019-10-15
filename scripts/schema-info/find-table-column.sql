91313532d8cb583cf9de45c75b41ab5319c8601f
Paul Anderson <paul.anderson@legendware.co.uk>
2019-10-15 16:50:36 +0100

    find-table-column.sql

--- scripts/schema-info/find-table-column.sql
+++ scripts/schema-info/find-table-column.sql
@@ -6,9 +6,9 @@
 --				type.
 --
 -------------------------------------------------------------------------------
-declare @findExact bit = 1
-declare @columnName varchar(max) --= 'status'
-declare @tableName varchar(max) = 'CourseTemplate'
+declare @findExact bit = 0
+declare @columnName varchar(max) = ''
+declare @tableName varchar(max) = 'taxru'
 declare @colType varchar(max)
 
 
@@ -17,9 +17,9 @@ select
 		, SCHEMA_NAME(tbl.[schema_id]) as [Schema]
 		, col.[name] as [Column]
 		, t.[name] as [DataType]
-		--, col.[max_length]
-		--, t.[max_length]
-		--, t.[user_type_id]
+		, col.[max_length]
+		, t.[max_length]
+		, t.[user_type_id]
 		
 from sys.columns col
 join sys.tables tbl on col.[object_id] = tbl.[object_id]
@@ -29,11 +29,11 @@ join sys.schemas s on t.[schema_id] = s.[schema_id]
 where
 	1=1
 	and (@colType is null or t.[name] = @colType)
-	and (@tableName is null 
+	and (IsNull(@tableName, '') = ''
 		or (@findExact = 1 and tbl.[name] = @tableName) 
 		or (@findExact = 0 and tbl.[name] like '%' + @tableName + '%')
 	)
-	and (@columnName is null 
+	and (IsNull(@columnName, '') = ''
 		or (@findExact = 1 and col.[name] = @columnName) 
 		or (@findExact = 0 and col.[name] like '%' + @columnName + '%')
 	)
