bd4efc907baed03ded33253521077b986ed0798a
andez2000 <andez2000@hotmail.com>
2020-09-14 09:44:14 +0100

    ocd changes for deadlock report

--- scripts/diagnostics/deadlocks/view-xml_deadlock_report-filesystem.sql
+++ scripts/diagnostics/deadlocks/view-xml_deadlock_report-filesystem.sql
@@ -14,22 +14,27 @@ AS (
 	select XFileData.XFile.value('@name', 'varchar(128)') as [fileName]
 	
 	from sys.dm_xe_session_targets st
-	inner join sys.dm_xe_sessions s ON s.address = st.event_session_address
+	inner join sys.dm_xe_sessions s on s.[address] = st.event_session_address
 	cross apply (
-		select Cast(target_data AS XML) as target_data_xml
+		select Cast(target_data as xml) as target_data_xml
 	) as TargetData
 	
 	cross apply 
 		TargetData.target_data_xml.nodes('//EventFileTarget/File') AS XFileData (XFile)
 	
-	where s.[NAME] = @extendedEventSessionName
+	where s.[name] = @extendedEventSessionName
 	and st.[target_name] = 'event_file'
 )
-select Convert(xml, event_data).query('/event/data/value/child::*'),
-       Convert(xml, event_data).value('(event[@name="xml_deadlock_report"]/@timestamp)[1]','datetime') as [execution_time]
+select deadlock_info.[xml_deadlock_report],
+       deadlock_info.[execution_time]
 
 FROM EventFileTargetFilename
 cross apply 
 	sys.fn_xe_file_target_read_file(EventFileTargetFilename.[fileName], null, null, null)
+cross apply (
+	select 
+		Convert(xml, event_data).query('/event/data/value/child::*') as xml_deadlock_report,
+		Convert(xml, event_data).value('(event[@name="xml_deadlock_report"]/@timestamp)[1]','datetime') as [execution_time]
+) as deadlock_info
+order by deadlock_info.execution_time desc
 ;
-
