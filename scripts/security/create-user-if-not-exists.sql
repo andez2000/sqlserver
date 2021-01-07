3b696fa177a7f80d0f5c7be6328fbe3c4744846d
andez2000 <andez2000@hotmail.com>
2021-01-07 14:05:19 +0000

    added create-user-if-not-exists.sql

--- scripts/security/create-user-if-not-exists.sql
+++ scripts/security/create-user-if-not-exists.sql
@@ -2,7 +2,7 @@ use [master]
 
 if not exists (select p.[name] from [master].sys.server_principals p where p.[name] = 'SOME_SQL_USERNAME')
 begin
-	create login [ADL_Report_Viewer] 
+	create login [SOME_SQL_USERNAME] 
 		  with password = N'SOME_PASSWORD'
 		, default_database = [master]
 		, check_expiration = off
