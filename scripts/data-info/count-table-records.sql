fee3445e4599b3051f1f858b4c0a0ae2335de5d3
andez2000 <andez2000@hotmail.com>
2021-07-06 08:24:22 +0100

    update count-table-records.sql

--- scripts/data-info/count-table-records.sql
+++ scripts/data-info/count-table-records.sql
@@ -1,21 +1,23 @@
-SELECT 
+select 
       t.[name] AS TableName
     , i.[name] as indexName
-    , p.[Rows]
-    , sum(a.total_pages) as TotalPages
-    , sum(a.used_pages) as UsedPages
-    , sum(a.data_pages) as DataPages
-    , (sum(a.total_pages) * 8) / 1024 as TotalSpaceMB
-    , (sum(a.used_pages) * 8) / 1024 as UsedSpaceMB
-    , (sum(a.data_pages) * 8) / 1024 as DataSpaceMB
-FROM 
+    , p.[rows]
+    , sum(a.[total_pages]) as TotalPages
+    , sum(a.[used_pages]) as UsedPages
+    , sum(a.[data_pages]) as DataPages
+    , (sum(a.[total_pages]) * 8) / 1024 as TotalSpaceMB
+    , (sum(a.[used_pages]) * 8) / 1024 as UsedSpaceMB
+    , (sum(a.[data_pages]) * 8) / 1024 as DataSpaceMB
+
+from 
     sys.tables t
-INNER JOIN      
-    sys.indexes i ON t.OBJECT_ID = i.object_id
-INNER JOIN 
-    sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
-INNER JOIN 
-    sys.allocation_units a ON p.partition_id = a.container_id
+
+inner join
+    sys.indexes i on t.[object_id] = i.[object_id]
+inner join
+    sys.partitions p on i.[object_id] = p.[object_id] AND i.[index_id] = p.[index_id]
+inner join 
+    sys.allocation_units a on p.[partition_id] = a.[container_id]
 
 WHERE 
     t.NAME NOT LIKE 'dt%' AND
